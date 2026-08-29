712fd595b36f000c7580ace13a5708df72aa66e7
jcseq-design <jcseq.design@gmail.com>
2026-08-29 21:36:59 +0100

    Centralize APF external links

--- js/site-links.js
+++ js/site-links.js
@@ -1,1 +1,8 @@
-const APF_LINKS = { instagram: 'https://www.instagram.com/fundraising.pt/', individualMembership: 'https://docs.google.com/forms/d/e/1FAIpQLSfI8sgz6AomvvBLIqcKar2PnfVemdt7TF1lghFIL7b_hUtt5g/formResponse', collectiveMembership: 'https://docs.google.com/forms/d/e/1FAIpQLSfCmUhVSENYV09viLGxUDMzOYxqap_6B33dgF13kA6kQMZFZQ/viewform' };
+const APF_LINKS = {
+  linkedin: "https://www.linkedin.com/company/associa%C3%A7%C3%A3o-portuguesa-de-fundraising/posts/?feedView=all",
+  instagram: "https://www.instagram.com/fundraising.pt/",
+  facebook: "https://www.facebook.com/people/Associa%C3%A7%C3%A3o-Portuguesa-de-Fundraising/61590159405180/",
+  individualMembership: "https://docs.google.com/forms/d/e/1FAIpQLSfI8sgz6AomvvBLIqcKar2PnfVemdt7TF1lghFIL7b_hUtt5g/viewform",
+  collectiveMembership: "https://docs.google.com/forms/d/e/1FAIpQLSfCmUhVSENYV09viLGxUDMzOYxqap_6B33dgF13kA6kQMZFZQ/viewform",
+  givingTuesday: "https://www.givingtuesday.pt/"
+};
